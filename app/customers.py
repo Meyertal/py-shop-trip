f3e167264b336f5ae987cc9f340086e2fa50eda3
meier <Meyertal2@gmail.com>
2024-04-01 12:44:58 +0300

    'Solution'

--- app/customers.py
+++ app/customers.py
@@ -9,17 +9,19 @@ class Customers:
     def __init__(self, customer: dict) -> None:
         self.name = customer["name"]
         self.product_cart = customer["product_cart"]
-        self.location = customer["location"]
+        self.home_location = customer["location"]
         self.money = customer["money"]
         self.car = Car(customer["car"])
         self.cost_trip = 0
         self.cheap_shop = None
+        self.customer_location = customer["location"]
 
     def customer_money(self) -> None:
         print(f"{self.name} has {self.money} dollars")
 
     def return_info(self) -> None:
         print(f"{self.name} rides to {self.cheap_shop.name}\n")
+        self.customer_location = self.cheap_shop.location
         print(f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
         print(f"Thanks, {self.name}, for your purchase!")
         print("You have bought:")
@@ -35,12 +37,13 @@ class Customers:
         print(f"Total cost is {total_cost} dollars")
         print("See you again!\n")
         print(f"{self.name} rides home")
+        self.customer_location = self.home_location
         money_left = round(self.money - self.cost_trip, 2)
         print(f"{self.name} now has {money_left} dollars\n")
 
-    def cheap_trip(self, shop: list, fuel_price: float) -> None:
-        for new_shop in shop:
-            fuel_cost = (math.dist(self.location, Shop(new_shop).location)
+    def cheap_trip(self, shops: list[Shop], fuel_price: float) -> None:
+        for new_shop in shops:
+            fuel_cost = (math.dist(self.home_location, Shop(new_shop).location)
                          * self.car.fuel_consumption / 100 * fuel_price)
             product_cost = sum(Shop(new_shop).products[i]
                                * self.product_cart[i]
